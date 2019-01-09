820897d0f8ebdf9452002c8007b284dd69bef783
JordanTheJet <morepencils@gmail.com>
2019-01-09 11:29:19 -0600

    styling update

--- src/app/restapi.service.ts
+++ src/app/restapi.service.ts
@@ -14,12 +14,62 @@ import{ Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class RestapiService {
+  userData = {
+    "Email": "",
+    "FirstName": "",
+    "LastName": "",
+    "ZipCode": 0,
+    "UserName": "",
+    "myPoints": 0,
+    "FullTasks": {},
+    "MedicalQ": {},
+    "currentTaskNum": 0
+  }
+  theData$: Observable < any >
   data: any
   HttpParams = new HttpParams();
   constructor(private cognitoService: CognitoService,
-              private http: HttpClient
+              private http: HttpClient,
+              private storage: Storage,
+              
               ) {}
+loadData(){
+    console.log("here")
+    this.theData$ = this.getData();
+    console.log("there")
+    this.theData$.subscribe((data) => {
+      console.log(data);
+      console.log(this.theData$)
+      this.userData.FirstName = data.Item.FirstName.S
+      console.log(this.userData)
+      console.log(this.userData.FirstName)
 
+      this.userData.LastName = data.Item.LastName.S
+      console.log(data.Item.LastName.S)
+
+      console.log(data.Item.ZipCode.S)
+      console.log(data.Item.ZipCode)
+      this.userData.ZipCode = data.Item.ZipCode.S
+      console.log("posted", this.userData.ZipCode)
+
+      this.userData.UserName = data.Item.UserName.S
+      console.log(this.userData.UserName)
+
+      this.userData.myPoints = parseInt(data.Item.myPoints.N)
+      console.log(this.userData.myPoints)
+      // gets task list
+      console.log(this.userData)
+
+    }, (err) => {
+      console.log(err)
+    })
+    this.finishTask()
+  }
+
+  finishTask(){
+    let currentTaskName= `task${this.userData.currentTaskNum}`
+    this.userData.FullTasks[currentTaskName].status = "done"
+  }
   postData(postData) {
     let myUser = this.cognitoService.getAuthenticatedUser();
     if (myUser === null) { // if there’s no data, leave function
